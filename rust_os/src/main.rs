4b71c451cfab37f46ff991f6a8356eafa129f36c
Robert <102342225+RobertxPearce@users.noreply.github.com>
2024-10-09 13:57:11 -0700

    disabled the standard library and defined panic implementation

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!();
+// Prevent Rust from loading the standard library
+#![no_std]
+
+use core::panic::PanicInfo;
+
+fn main() {}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
