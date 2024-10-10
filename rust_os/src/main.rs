6f271cd8fcbd15bd8172320d07b99f8cca8a40a3
Robert <robertpearcetv@gmail.com>
2024-10-09 22:03:30 -0700

    print 'Hello World!' directly to the VGA buffer using raw pointers and an unsafe block

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -3,10 +3,24 @@
 
 use core::panic::PanicInfo;
 
+// static byte array storing each char as its ascii representation
+static HELLO: &[u8] = b"Hello World!";
+
 // this function is the entry point, the linker looks for
 // a function named '_start' by default
 #[no_mangle] // use no_mangle attribute to prevent name mangling
 pub extern "C" fn _start() -> ! {
+    // set mutable raw pointer to VGA test buffer address (0xb8000 in x86)
+    let vga_buffer= 0xb8000 as *mut u8;
+    // for loop to iterate over each byte in 'HELLO'
+    for (i, &byte) in HELLO.iter().enumerate() {
+        // unsafe block around all memory writes
+        unsafe {
+            // write the byte and color to the VGA buffer
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
 
