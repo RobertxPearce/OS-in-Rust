00471887f31228c5efae8fe4b31ad29afd3391f4
Robert <robertpearcetv@gmail.com>
2024-10-09 19:48:38 -0700

    complete freestanding Rust binary implementation

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -1,10 +1,16 @@
-// Prevent Rust from loading the standard library
-#![no_std]
+#![no_std] // prevent Rust from loading the standard library
+#![no_main] // tell compiler we don't want to use the normal entry point chain
 
-fn main() {}
-
-// Define panic handler function
 use core::panic::PanicInfo;
+
+// this function is the entry point, the linker looks for
+// a function named '_start' by default
+#[no_mangle] // use no_mangle attribute to prevent name mangling
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+// define panic handler function
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
